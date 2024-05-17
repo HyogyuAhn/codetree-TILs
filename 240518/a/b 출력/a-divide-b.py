8960a48c5cf036ac84cf1c2fc687b26985116a1a
Hyogyu Ahn <41979177+hyogyu04@users.noreply.github.com>
2024-05-18 03:08:06 +0900

    Title: a/b 출력, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240518/a/b 출력/a-divide-b.py
+++ 240518/a/b 출력/a-divide-b.py
@@ -1,8 +1,16 @@
-import random
-from decimal import Decimal, getcontext, ROUND_DOWN
-
 a, b = map(int, input().split())
-getcontext().prec = 20
-result = Decimal(a) / Decimal(b)
-rounded_result = result.quantize(Decimal('0.00000000000000000000'), rounding=ROUND_DOWN)
-print(f"{rounded_result}")
+result = a / b
+result_str = str(result)
+decimal_point_index = result_str.find('.')
+if decimal_point_index == -1:
+    result_str += '.00000000000000000000'
+else:
+    decimal_length = len(result_str) - decimal_point_index - 1
+    while decimal_length < 20:
+        result_str += '0'
+        decimal_length += 1
+    if decimal_length > 20:
+        result_str = result_str[:decimal_point_index + 1 + 20]
+integer_part, decimal_part = result_str.split('.')
+final_result = integer_part + '.' + decimal_part[:21]
+print(f"{final_result}")
