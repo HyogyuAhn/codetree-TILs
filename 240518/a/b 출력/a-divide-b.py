5a80e6e56b4f503f563dbd1f542da66c007a2b75
Hyogyu Ahn <41979177+hyogyu04@users.noreply.github.com>
2024-05-18 03:10:28 +0900

    Title: a/b 출력, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240518/a/b 출력/a-divide-b.py
+++ 240518/a/b 출력/a-divide-b.py
@@ -2,15 +2,15 @@ a, b = map(int, input().split())
 result = a / b
 result_str = str(result)
 decimal_point_index = result_str.find('.')
-if decimal_point_index == -1:
-    result_str += '.00000000000000000000'
-else:
-    decimal_length = len(result_str) - decimal_point_index - 1
+decimal_length = len(result_str) - decimal_point_index - 1
+if decimal_point_index != -1:
     while decimal_length < 20:
         result_str += '0'
         decimal_length += 1
     if decimal_length > 20:
         result_str = result_str[:decimal_point_index + 1 + 20]
+else:
+    result_str += '.00000000000000000000'
 integer_part, decimal_part = result_str.split('.')
-final_result = integer_part + '.' + decimal_part[:21]
+final_result = integer_part + '.' + decimal_part[:20]
 print(f"{final_result}")
