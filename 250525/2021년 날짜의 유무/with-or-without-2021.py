9a79d02dac61cb1a6b611efc6b2539b91b40235e
Hyogyu Ahn <41979177+HyogyuAhn@users.noreply.github.com>
2025-05-25 01:55:20 +0900

    Title: 2021년 날짜의 유무, Time: 82ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250525/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,22 @@
+M, D = map(int, input().split())
+
+# Please write your code here.
+dayMax = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+def isExistDate(M, D):
+    return "Yes" if D <= dayMax[M] else "No"
+
+print(isExistDate(M, D))
