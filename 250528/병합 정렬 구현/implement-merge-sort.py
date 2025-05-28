5e7659ac3faa64babd3ee16dedbcd411871c75bb
Hyogyu Ahn <41979177+HyogyuAhn@users.noreply.github.com>
2025-05-28 14:57:31 +0900

    Title: 병합 정렬 구현, Time: 165ms, Memory: 24MB, Status: Passed - Codetree

--- 250528/병합 정렬 구현/implement-merge-sort.py
+++ 250528/병합 정렬 구현/implement-merge-sort.py
@@ -1,39 +1,38 @@
 n = int(input())
 arr = list(map(int, input().split()))
+temp = [0] * n
 
-# Please write your code here.
-def mergeSort(arr, low, high):
+def mergeSort(arr, temp, low, high):
     if low < high:
         mid = (low + high) // 2
-        mergeSort(arr, low, mid)
-        mergeSort(arr, mid + 1, high)
-        merge(arr, low, mid, high)
-        
-def merge(arr, low, mid, high):
-    i, j = low, mid + 1
-    k = low
-    merged_arr = [0] * (high + 1)
+        mergeSort(arr, temp, low, mid)
+        mergeSort(arr, temp, mid + 1, high)
+        merge(arr, temp, low, mid, high)
+
+def merge(arr, temp, low, mid, high):
+    i, j, k = low, mid + 1, low
+
     while i <= mid and j <= high:
         if arr[i] <= arr[j]:
-            merged_arr[k] = arr[i]
-            k += 1; i+= 1
+            temp[k] = arr[i]
+            i += 1
         else:
-            merged_arr[k] = arr[j]
-            k += 1; j += 1
-    
+            temp[k] = arr[j]
+            j += 1
+        k += 1
+
     while i <= mid:
-        merged_arr[k] = arr[i]
-        k += 1; i += 1
+        temp[k] = arr[i]
+        i += 1
+        k += 1
 
     while j <= high:
-        merged_arr[k] = arr[j]
-        k += 1; j += 1
+        temp[k] = arr[j]
+        j += 1
+        k += 1
 
-    for k in range(low, high + 1):
-        arr[k] = merged_arr[k]
+    for idx in range(low, high + 1):
+        arr[idx] = temp[idx]
 
-    return arr
-
-mergeSort(arr, 0, n - 1)
-for d in arr:
-    print(d, end = ' ')
+mergeSort(arr, temp, 0, n - 1)
+print(*arr)
